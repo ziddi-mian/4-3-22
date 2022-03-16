00b0a1c108a6d476ded4813896deb2d9c555ae77
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-03-16 08:38:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="..."> </img>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="..."></img>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="..."></img>
+    </div>
+  </div>
+</div>
     </div>
   );
 }
